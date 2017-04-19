c6191caa48f065a7d68d6bebce490ab48be164c5
ZanSchaffer <zan.schaffer@gmail.com>
2017-04-19 17:39:15 +0200

    Finished base

---
+++ projekt1.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void write(int n, int slika[][n]){
+    for(int i=0; i < n; i++){
+        for(int j=0; j < n; j++){
+            printf("%d ", slika[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
+
+int giveColor(int n, int slika[][n], int i, int j){
+    if (i == -1) i = n;
+    if (j == -1) j = n;
+    i = i % n;
+    j = j % n;
+    return slika[i][j];
+}
+
+int choose(int n, int slika[][n], int i, int j){
+    int temp[8];
+    temp[0] = giveColor(n, slika, i-1, j-1);
+    temp[1] = giveColor(n, slika, i,   j-1);
+    temp[2] = giveColor(n, slika, i-1, j);
+    temp[3] = giveColor(n, slika, i+1, j-1);
+    temp[4] = giveColor(n, slika, i-1, j-1);
+    temp[5] = giveColor(n, slika, i,   j+1);
+    temp[6] = giveColor(n, slika, i+1, j);
+    temp[7] = giveColor(n, slika, i+1, j+1);
+
+    return temp[rand() % 8];
+}
+
+int main(){
+    srand(time(NULL));
+    
+    int slika[5][5]={  
+        {0,1,2,3,4},
+        {5,6,7,8,9},
+        {0,1,2,3,4},
+        {5,6,7,8,9},
+        {0,2,4,6,8}
+    };
+
+
+    int novaSlika[5][5];
+    int n = 5;
+    write(n,slika);
+
+    for(int count=0;1;count++){
+        int barva = slika[0][0];
+        int zmaga = 1;
+        if(count % 2 == 0){    
+            for(int i=0; i < n; i++){
+                for(int j=0; j < n; j++){
+                    novaSlika[i][j] = choose(n-1,slika,i,j);
+                    if(novaSlika[i][j] != barva){
+                        zmaga=0;
+                    }
+                }
+            }
+            write(n,novaSlika);
+            
+        }else{
+            for(int i=0; i < n; i++){
+                for(int j=0; j < n; j++){
+                    slika[i][j] = choose(n-1,novaSlika,i,j);
+                    if(slika[i][j] != barva){
+                        zmaga=0;
+                    }
+                }
+            }
+            write(n,slika);
+        }
+        if(zmaga){
+            break;
+        }
+    }
+}
